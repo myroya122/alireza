712271a2e9dcd51e3cf769d43d244b7c88bdd656
myroya122 <54057124+myroya122@users.noreply.github.com>
2025-03-05 02:25:58 +0330

    Update Kharej.sh

--- Kharej.sh
+++ Kharej.sh
@@ -6,11 +6,18 @@ read SERVER2_IP
 
 # دستورات مربوط به سرور 1
 echo "در حال ایجاد تونل روی سرور 1 ..."
-ip tunnel add tun6to4 mode sit ttl 254 remote $SERVER2_IP
-ip link set tun6to4 up
-ip addr add fc01::1/64 dev tun6to4
-ip tunnel add gre1 mode ip6gre remote fc01::2 local fc01::1
-ip link set gre1 up
-ip addr add 10.10.15.1/30 dev gre1
-ip route add default via 10.10.15.2
-sysctl -w net.ipv4.ip_forward=1
+
+# ایجاد تونل 6to4
+sudo ip tunnel add tun6to4 mode sit ttl 254 remote $SERVER2_IP
+sudo ip link set tun6to4 up
+sudo ip addr add fc01::1/64 dev tun6to4
+
+# ایجاد تونل GRE
+sudo ip tunnel add gre1 mode ip6gre remote fc01::2 local fc01::1
+sudo ip link set gre1 up
+sudo ip addr add 10.10.15.1/30 dev gre1
+
+# فعال‌سازی forwarding IPv4
+sudo sysctl -w net.ipv4.ip_forward=1
+
+echo "تنظیمات تونل با موفقیت انجام شد."
