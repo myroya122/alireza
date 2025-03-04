33acf9d2fe3a4af59b059f0adb9c5904a45e836e
myroya122 <54057124+myroya122@users.noreply.github.com>
2025-03-05 02:39:01 +0330

    Update Iran.sh

--- Iran.sh
+++ Iran.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# گرفتن آی‌پی سرور 1 (سرور خارجی)
+# دریافت آی‌پی سرور 1 به طور تعاملی
 echo "لطفاً آی‌پی سرور 1 (سرور خارجی) را وارد کنید:"
 read SERVER1_IP
 
 # دستورات مربوط به سرور 2
 echo "در حال ایجاد تونل روی سرور 2 ..."
-ip tunnel add tun6to4 mode sit ttl 254 remote $SERVER1_IP
-ip link set tun6to4 up
-ip addr add fc01::2/64 dev tun6to4
-ip tunnel add gre1 mode ip6gre remote fc01::1 local fc01::2
-ip link set gre1 up
-ip addr add 10.10.15.2/30 dev gre1
-ip route add default via 10.10.15.1
-sysctl -w net.ipv4.ip_forward=1
+
+# ایجاد تونل 6to4
+sudo ip tunnel add tun6to4 mode sit ttl 254 remote $SERVER1_IP
+sudo ip link set tun6to4 up
+sudo ip addr add fc01::2/64 dev tun6to4
+
+# ایجاد تونل GRE
+sudo ip tunnel add gre1 mode ip6gre remote fc01::1 local fc01::2
+sudo ip link set gre1 up
+sudo ip addr add 10.10.15.2/30 dev gre1
+
+# فعال‌سازی forwarding IPv4
+sudo sysctl -w net.ipv4.ip_forward=1
+
+echo "تنظیمات تونل با موفقیت انجام شد."
